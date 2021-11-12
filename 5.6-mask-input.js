6e7bcc775bb2e4bc93fab614ef837a53828466bc
sarit-chaet-hudis <sarit.chh@gmail.com>
2021-11-12 12:20:12 +0200

    mask input

--- 5.6-mask-input.js
+++ 5.6-mask-input.js
@@ -1,3 +1,7 @@
-function maskify (str) {
-    // function maskify, which changes all but the last four characters into '#'.
-}
+const maskify = (str) => {
+  // function maskify, which changes all but the last four characters into '#'.
+  return str.length <= 4 
+  ? str 
+  : "#".repeat(str.length - 4) + str.slice(-4);
+};
+console.log(maskify("_ertrtret"));
